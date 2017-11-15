d25d6ac2be82a33c44ba8815d3081d5c85601356
JohnRey <383094403@qq.com>
2017-11-15 18:59:01 +0800

    update

--- protocol/msg.bat
+++ protocol/msg.bat
@@ -1,4 +1,4 @@
-protogen -i:LoginInfo.proto -o:LoginInfo.cs
-protoc --go_out=. LoginInfo.proto
-protoc --plugin=protoc-gen-lua="..\pluginLua\protoc-gen-lua.bat" --lua_out=. LoginInfo.proto
+protogen -i:login.proto -o:Login.cs
+protoc --go_out=. login.proto
+protoc --plugin=protoc-gen-lua="..\pluginLua\protoc-gen-lua.bat" --lua_out=. login.proto
 pause
